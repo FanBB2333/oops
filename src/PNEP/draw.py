ecff2b78c5ebdba22d1161e04a75b9c243c562d7
l1ght <2504082957@qq.com>
2021-12-13 20:49:02 +0800

    update draw

--- src/PNEP/draw.py
+++ src/PNEP/draw.py
@@ -52,26 +52,49 @@ with open(SBART_txt, 'r') as f:
         times = [l.count(n) for n in occur_planning]
         summarized_times2.append(np.sum(times))
 
+
+summarized_SAMSum_lens = []  # length
+summarized_SAMSum_times = []  # 人物出现次数
+
+with open(target_txt, 'r') as f:
+    lines = f.readlines()
+    for idx, l in enumerate(lines):
+        summarized_SAMSum_lens.append(len(l.split()))
+        occur_planning = occur_plannings[idx]
+        times = [l.count(n) for n in occur_planning]
+        summarized_SAMSum_times.append(np.sum(times))
+
 if __name__ == '__main__':
     # using length
     abs_deg_PNEP = np.array(summarized_lens) / np.array(original_lens)
     abs_deg_SBART = np.array(summarized_lens2) / np.array(original_lens)
-    plt.subplot(2, 2, 1)
+    plt.subplot(3, 2, 1)
     plt.title('PNEP')
     plt.ylabel('times~length ratio')
-    plt.hist(abs_deg_PNEP, bins=20, label='PNEP')
-    plt.subplot(2, 2, 2)
+    plt.hist(abs_deg_PNEP, bins=20, label='PNEP', density=True)
+    plt.subplot(3, 2, 2)
     plt.title('SBART')
-    plt.hist(abs_deg_SBART, bins=20, label='SBART')
+    plt.hist(abs_deg_SBART, bins=20, label='SBART', density=True)
 
     # using times
     abs_deg_PNEP = np.array(summarized_times) / np.array(original_times)
     abs_deg_SBART = np.array(summarized_times2) / np.array(original_times)
-    plt.subplot(2, 2, 3)
+    plt.subplot(3, 2, 3)
     plt.ylabel('times~names freq ratio')
-    plt.hist(abs_deg_PNEP, bins=20, label='PNEP')
-    plt.subplot(2, 2, 4)
+    plt.hist(abs_deg_PNEP, bins=20, label='PNEP', density=True)
+    plt.subplot(3, 2, 4)
 
-    plt.hist(abs_deg_SBART, bins=20, label='SBART')
+    plt.hist(abs_deg_SBART, bins=20, label='SBART', density=True)
+    # plt.savefig('fig.png')
+
+    # fg = plt.figure()
+    plt.subplot(3, 2, 5)
+    plt.xlabel('length')
+
+    plt.hist(np.array(summarized_SAMSum_lens) / np.array(original_lens), bins=20, label='SAMSum', density=True)
+
+    plt.subplot(3, 2, 6)
+    plt.xlabel('names freq')
+    plt.hist(np.array(summarized_SAMSum_times) / np.array(original_times), bins=20, label='SAMSum', density=True)
     plt.savefig('fig.png')
     plt.show()
